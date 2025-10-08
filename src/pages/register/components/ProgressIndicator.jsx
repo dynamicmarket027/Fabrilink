6fe8b1d48aa7d900256966a7162cba4e9c6183c3
dynamicmarket027 <dynamicmarket027@gmail.com>
2025-10-08 19:51:42 +0200

    mam rutas con puntoa

--- src/pages/register/components/ProgressIndicator.jsx
+++ src/pages/register/components/ProgressIndicator.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import Icon from 'src/components/AppIcon';
-import Button from 'src/components/ui/Button';
+import Icon from '../../../components/AppIcon';
+import Button from '../../../components/ui/Button';
 import RegistrationForm from './RegistrationForm';
 import ProgressIndicator from './ProgressIndicator';
 import SocialRegistration from './SocialRegistration';
